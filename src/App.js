881c6a21b1463b37ad4e37766503180237998c41
Sumedh Raj <rsumedh0077@gmail.com>
2021-01-16 12:49:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import MasonryLayout from './MasonaryLayout';
 
 function App() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <div>
+      <h1>Hello</h1>
+      <MasonryLayout columns={2} gap={25}>
+        {
+          [...Array(12).keys()].map(key => {
+            const height = 200 + Math.ceil(Math.random() * 300);
+
+            return (
+              <div style={{
+                height: `${height}px`, display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                background: "#efefef"
+              }} />
+            )
+          })
+        }
+      </MasonryLayout>
+    </div>);
 }
 
 export default App;
